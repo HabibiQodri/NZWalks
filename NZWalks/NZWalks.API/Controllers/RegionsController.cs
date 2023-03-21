ab05f761d0affebe46d792974c1cea524eaed172
RenZasai <habibiqodri0511@gmail.com>
2023-03-21 16:43:44 +0700

    End Of Section 3

---
+++ NZWalks/NZWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,49 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    // [Route("Regions")] == [Route("[controller]")]
+    [Route("[controller]")]
+    public class RegionsController : Controller
+    {
+        private readonly IRegionRepository regionRepository;
+        private readonly IMapper mapper;
+
+        public RegionsController(IRegionRepository regionRepository , IMapper mapper)
+        {
+            this.regionRepository = regionRepository;
+            this.mapper = mapper;
+        }
+
+        [HttpGet]
+       public async Task<IActionResult> GetAllRegions()
+        {
+            var regions = await regionRepository.GetAllAsync();
+
+            // return DTO Regions
+                //var regionsDTO = new List<RegionDto>();
+                //regions.ToList().ForEach(region => {
+                //    var regionDto = new RegionDto()
+                //    {
+                //        Id = region.Id,
+                //        Code = region.Code,
+                //        Name = region.Name,
+                //        Area = region.Area,
+                //        Lat = region.Lat,
+                //        Long = region.Long,
+                //        Population  = region.Population,        
+                //    };  
+                //    regionsDTO.Add(regionDto);  
+                //});
+            
+            var regionsDTO = mapper.Map<List<RegionDto>>(regions);
+            
+            return Ok(regionsDTO);
+        } 
+    }
+}
---
+++ NZWalks/NZWalks.API/Profiles/RegionsProfile.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using NZWalks.API.Models.Domain;
+using NZWalks.API.Models.DTO;
+
+namespace NZWalks.API.Profiles
+{
+    public class RegionsProfile : Profile
+    {
+        public RegionsProfile() {
+            CreateMap<Region, RegionDto>()
+                .ReverseMap();
+
+                // Ketika ingin mengambalikan propety dto ke seperti sumbernya 
+                // .ReverseMap();
+                
+                // ketika properti tidak memeliki nama yg sama antara sumber dan tujuan mapper
+                // .ForMember(dest => dest.Id, options => options.MapFrom(src => src.RegionId));
+        }
+    }
+}
